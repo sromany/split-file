b7b859832e3fc535bf6d10f125c7211f1139c767
Stéphane <stephrm972@gmail.com>
2023-05-04 10:47:09 +0200

    upate gitignore and remove unnecessary comment

--- src/main.rs
+++ src/main.rs
@@ -75,12 +75,3 @@ fn main() -> std::io::Result<()> {
     Ok(())
 }
 
-//  Options:
-//  -f, --file-path=<s>                         Path to csv file to be split
-//  -n, --new-file-name=<s>                     Name of the new files. This will be appended with an incremented number (default: split)
-//  -i, --include-headers, --no-include-headers Include headers in new files (default: true)
-//  -l, --line-count=<i>                        Number of lines per file (default: 1)
-//  -d, --delimiter=<s>                         Charcter used for Col. Sep. (Default: ,)
-//  -r, --remove-columns                        Specify column names to be removed during processing in remove_coluns.txt
-//  -c, --include-remainders                    Include remainder rows in the split files (default: false). Example: if there are 1030 rows in a csv file and will be split in 100 rows, the remaining 30 rows will be stored in a new file
-//  -h, --help                                  Show this message
